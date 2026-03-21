2e38de113f7f25d5e46b6c0804848b0009882052
Martin Mihle Nygaard <martin@mihle.no>
2026-03-21 11:35:38 +0100

    fix: letterboxing issue

--- Tracker/Features/ContentView.swift
+++ Tracker/Features/ContentView.swift
@@ -1,46 +1,50 @@
-import SwiftUI
 import EventKitUI
+import SwiftUI
 
 struct ContentView: View {
-    @Bindable private var viewModel = ContentViewModel.shared
+  @Bindable private var viewModel = ContentViewModel.shared
 
-    var body: some View {
-        NavigationView {
-            ZStack {
-                Color.clear
-                    .contentShape(Rectangle())
-                    .onTapGesture {
-                        withAnimation {
-                            viewModel.showTextField.toggle()
-                        }
-                    }
-                
-                VStack {
-                    if !viewModel.showTextField {
-                        Text(viewModel.eventName)
-                            .font(.headline)
-                            .foregroundColor(.secondary)
-                    }
-                    TimerView(viewModel: viewModel).background(.clear)
-                    if viewModel.showTextField {
-                        EventFormView(viewModel: viewModel)
-                    }
-                }
-                .sheet(isPresented: $viewModel.showAll) {
-                    ShowAllView(viewModel: viewModel)
-                }
-                .onAppear(perform: viewModel.fetchEvents)
-                .onChange(of: viewModel.events, { _, _ in
-                    viewModel.fetchEvents()
-                })
-                .sheet(item: $viewModel.selectedEvent) { event in
-                    EventDetailView(event: event, events: $viewModel.events, onDelete: viewModel.deleteEventFromDetail)
-                }
+  var body: some View {
+    NavigationView {
+      ZStack {
+        Color.clear
+          .contentShape(Rectangle())
+          .onTapGesture {
+            withAnimation {
+              viewModel.showTextField.toggle()
             }
+          }
+
+        VStack {
+          if !viewModel.showTextField {
+            Text(viewModel.eventName)
+              .font(.headline)
+              .foregroundColor(.secondary)
+          }
+          TimerView(viewModel: viewModel).background(.clear)
+          if viewModel.showTextField {
+            EventFormView(viewModel: viewModel)
+          }
         }
+        .sheet(isPresented: $viewModel.showAll) {
+          ShowAllView(viewModel: viewModel)
+        }
+        .onAppear(perform: viewModel.fetchEvents)
+        .onChange(
+          of: viewModel.events,
+          { _, _ in
+            viewModel.fetchEvents()
+          }
+        )
+        .sheet(item: $viewModel.selectedEvent) { event in
+          EventDetailView(
+            event: event, events: $viewModel.events, onDelete: viewModel.deleteEventFromDetail)
+        }
+      }
     }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
